f989469e7433454c6ef69282380ba9c0913ffaf7
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-11-05 20:11:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,12 @@ end
 def get_japanese_emoticon(yaml_file, emoticon)
   yaml_file = load_library(yaml_file)
   yaml_file.each do |key, value|
-    
+    if value[:english] == emoticon
+      return value[:japanese]
+    else
+      return 'Sorry, emoticon not found.'
+    end
+  end
 end
 
 def get_english_meaning
